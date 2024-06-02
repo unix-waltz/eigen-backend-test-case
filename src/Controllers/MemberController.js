6c8386d7db408f9b66de88081dd96fc2a4c84815
unix-waltz <riosraskaa@gmail.com>
2024-06-02 14:22:03 +0700

    finally ( end ) add return book and fix some bug n error

--- src/Controllers/MemberController.js
+++ src/Controllers/MemberController.js
@@ -42,9 +42,12 @@ try {
     where:{id:+userid},
     include :{transaction:true}
   }) 
-
+  const transactionuser = await prisma.user.findUnique({
+    where:{id:+userid},
+    include :{transaction:{where:{status:"ACTIVE"}}}
+  }) 
   if(!founduser) return response({response:res,msg : "user not Found!",code : 404})
-    const transactionCount = founduser.transaction.length;
+    const transactionCount = transactionuser.transaction.length;
   if(transactionCount >= 2) return response({response:res,msg : "you cannot borrow more than 2 books!" ,code:400});
 
 const foundbook = await prisma.books.findUnique({
@@ -55,6 +58,18 @@ const foundbook = await prisma.books.findUnique({
         return response({response:res,msg : "Book cannot be borrowed!" ,code:400});
       }
       if (founduser.penalty == 'TRUE' || founduser.penalty !== "FALSE") {
+        const waktuReferensi = new Date(founduser.penaltyAt);
+        const waktuSaatIni = new Date();
+        
+        const selisihHari = Math.floor((waktuSaatIni - waktuReferensi) / (1000 * 60 * 60 * 24 * 3))
+        if (selisihHari > 3) {
+            await prisma.user.update({
+                data:{penalty:FALSE},
+                where:{id:+userid}
+            })
+            console.log("un penalty :v")
+        } 
+        
         return response({response:res,msg : "Book cannot be borrowed,You Have a Penalty!" ,code:400});
       }
     const transaction = await prisma.transaction.create({
@@ -78,5 +93,67 @@ const foundbook = await prisma.books.findUnique({
     return response({response:res,msg : error,code : 500})
 }
     },
+
+    ReturnBook:async(req,res)=>{
+const {userid,bookid} = req.body
+try {
+    const founduser = await prisma.user.findUnique({
+        where:{id:+userid},
+        include :{transaction:{where:{status : "ACTIVE"}}}
+      }) 
+      if(!founduser) return response({response:res,msg : "user not Found!",code : 404})
+    const foundbook = await prisma.books.findUnique({
+            where:{id:+bookid},
+            include :{transaction:{where:{status : "ACTIVE"}}}
+          }) 
+      if(!foundbook) return response({response:res,msg : "Book not Found!",code : 404})
+  
+
+// check penalty
+// foundbook.transaction.forEach((transaction, index) => {
+//     res.send("Transaction", index + 1);
+//     res.send(transaction);
+// });
+const old_transc = await prisma.books.findUnique({
+    where: { id: +bookid },
+    include: { 
+        transaction: {
+            where: { status: "ACTIVE" },
+            orderBy: { borrowedAt: 'asc' }
+        }
+    }
+});
+
+// return res.send(old_transc)
+
+const waktuReferensi = new Date(old_transc.transaction[0].borrowedAt);
+const waktuSaatIni = new Date();
+
+const selisihHari = Math.floor((waktuSaatIni - waktuReferensi) / (1000 * 60 * 60 * 24 * 7))
+if (selisihHari > 7) {
+    await prisma.user.update({
+        data:{penalty:'TRUE',penaltyAt: new Date()},
+        where:{id:+userid}
+    })
+    console.log("You Got Penalty")
+} 
+const bookup = await prisma.books.update({
+    data :{status:"CANBEBORROWED"},
+    where :{id:+bookid}
+})
+const tId = foundbook.transaction[0].id;
+// return res.send(`${tId}`)
+const transckup = await prisma.transaction.update({
+    data :{status:"RETURNED",returnedAt: new Date()},
+    where :{id:tId}
+})
+return response({response:res,data:{transaction :transckup,book:bookup},msg: "Success Return the book"})
+
+
+} catch (error) {
+    console.log(error.message)
+    return response({response:res,msg : error,code : 500})
+}
+    }
 }
 export default Controller
--- src/Routes/MemberRoutes.js
+++ src/Routes/MemberRoutes.js
@@ -3,5 +3,6 @@ import Controller from './../Controllers/MemberController.js'
 const r = Router()
 r.get('/show/allbooks',Controller.ShowAllBooks)
 r.get('/show/allmembers',Controller.ShowAllMember)
-r.post('/borrowbook',Controller.BorrowBooks)
+r.post('/borrow-book',Controller.BorrowBooks)
+r.post('/return-book',Controller.ReturnBook)
 export default r
